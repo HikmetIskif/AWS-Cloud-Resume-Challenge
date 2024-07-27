41dee3828eee45d32db5c9f3f4397848bc4e4018
HikmetIskif <gangamhikmet4@gmail.com>
2024-07-27 18:08:40 +0300

    Uploaded lambda function

---
+++ lambda/lambda_function.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('hikmet-cloud-resume-challenge')
+def lambda_handler(event, context):
+    response = table.get_item(Key={
+        'ID':'1'
+    })
+    views = response['Item']['views']
+    views = views + 1
+    print(views)
+    response = table.put_item(Item={
+            'ID':'1',
+            'views': views
+    })
+
+    return views
